e140a6fd5c447237899c1952365d416d69a6248d
zoudong <692737146@qq.com>
2017-11-29 00:59:08 +0800

    init

---
+++ src/main/java/com/zoudong/okbitpay/OkbitpayApplication.java
@@ -0,0 +1,12 @@
+package com.zoudong.okbitpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkbitpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkbitpayApplication.class, args);
+	}
+}
