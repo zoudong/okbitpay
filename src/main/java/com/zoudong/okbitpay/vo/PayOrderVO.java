be6c8641be133734a1a6f90460e655d1d5c45020
zoudong <692737146@qq.com>
2017-11-30 15:13:00 +0800

    dto po vo

---
+++ src/main/java/com/zoudong/okbitpay/vo/PayOrderVO.java
@@ -0,0 +1,47 @@
+package com.zoudong.okbitpay.vo;
+
+import com.zoudong.okbitpay.util.result.BaseEntity;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+public class PayOrderVO extends BaseEntity{
+
+    private Long code;
+
+    private BigDecimal amount;
+
+    private String receiveAddress;
+
+    private String sendAddress;
+
+    private Date payTime;
+
+    private Long retryCount;
+
+    private Date lastRetryTime;
+
+    private String payStatus;
+
+    private String payDescription;
+
+    private Long productId;
+
+    private String productName;
+
+    private Long productNumber;
+
+    private String accountCode;
+
+    private String accountName;
+
+    private String status;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+
+}
