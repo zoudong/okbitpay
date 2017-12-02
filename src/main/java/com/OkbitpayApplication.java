7c9ae1e8fd32ec8f8d4d72fa9cb9f6be7979a912
zoudong <692737146@qq.com>
2017-12-03 00:57:24 +0800

    跨域请求

--- src/main/java/com/OkbitpayApplication.java
+++ src/main/java/com/OkbitpayApplication.java
@@ -1,9 +1,15 @@
 package com;
 
+import com.zoudong.okbitpay.filter.CORSFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableWebMvc
 @SpringBootApplication
@@ -11,5 +17,16 @@ public class OkbitpayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OkbitpayApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean filterRegistrationBean() {
+		FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+		CORSFilter corsFilter = new CORSFilter();
+		registrationBean.setFilter(corsFilter);
+		List<String> urlPatterns = new ArrayList<String>();
+		urlPatterns.add("/*");
+		registrationBean.setUrlPatterns(urlPatterns);
+		return registrationBean;
 	}
 }
---
+++ src/main/java/com/zoudong/okbitpay/filter/CORSFilter.java
@@ -0,0 +1,35 @@
+package com.zoudong.okbitpay.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 用于支持跨域请求
+ */
+public class CORSFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        String origin = (String) servletRequest.getRemoteHost() + ":" + servletRequest.getRemotePort();
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "36000");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        //response.setHeader("content-type","application/json;charset=utf-8");
+
+        response.setHeader("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept,Authorization");
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
