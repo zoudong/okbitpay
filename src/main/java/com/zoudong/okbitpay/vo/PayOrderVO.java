1b3512478416293829ebba56c014e81f428d4dd6
zoudong <692737146@qq.com>
2017-12-03 00:07:26 +0800

    入参检查

--- src/main/java/com/zoudong/okbitpay/vo/PayOrderVO.java
+++ src/main/java/com/zoudong/okbitpay/vo/PayOrderVO.java
@@ -8,7 +8,7 @@ import java.math.BigDecimal;
 import java.util.Date;
 
 @Data
-public class PayOrderVOBase extends BasePageVO {
+public class PayOrderVO extends BasePageVO {
 
     private Long id;
 
